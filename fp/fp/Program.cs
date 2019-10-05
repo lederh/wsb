3fd59700f2c0275ae06d25193d96adf703b94e1f
lederh <lederh27@gmail.com>
2019-10-05 09:34:18 +0200

    Pierwszy program

---
+++ fp/fp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace fp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Imię: "); //komentarz w jednej linii
+            Console.WriteLine("Nazwisko: ");
+            /*komentarz
+             * w
+             * wie
+             * lu
+             * liniach
+             */
+
+            string name = "Anna";
+            Console.WriteLine("Masz na imię: " + name);
+            Console.Write("Masz na imię: ");
+            Console.WriteLine(name);
+
+            Console.WriteLine("Masz na imię: {0}", name);
+
+            int age = 20;
+            sbyte age1 = 20;
+            Console.WriteLine("Wiek: {0}, Imię: {1}", age, name);
+            Console.ReadKey();
+        }
+    }
+}
